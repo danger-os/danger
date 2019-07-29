36f985e3dbd9f377eabc794b0af65ddbb1747580
Linux User <dave@arran.fitz.private.cam.ac.uk>
2019-07-29 23:35:11 +0000

    Tidy up GPIO code a little and redo bracketing

--- src/gpio/gpio_arm.d
+++ src/gpio/gpio_arm.d
@@ -1,6 +1,28 @@
 module gpio_arm;
 
-__gshared byte* GPIO_BASE = cast(byte*) 0x3f200000;
+enum byte* GPIO_BASE = cast(byte*) 0x3f200000;
+
+enum GPIO_FSEL : byte*
+{
+    FSEL0 = GPIO_BASE + 0x00,
+    FSEL1 = GPIO_BASE + 0x04,
+    FSEL2 = GPIO_BASE + 0x08,
+    FSEL3 = GPIO_BASE + 0x0c,
+    FSEL4 = GPIO_BASE + 0x10,
+    FSEL5 = GPIO_BASE + 0x14,
+}
+
+enum GPIO_FSEL_MODE : byte
+{
+    INP = 0b000,
+    OUT = 0b001,
+    ALT0 = 0b100,
+    ALT1 = 0b101,
+    ALT2 = 0b110,
+    ALT3 = 0b111,
+    ALT4 = 0b011,
+    ALT5 = 0b010,
+}
 
 @nogc nothrow @trusted void set_mode(byte pin, byte mode)
 {
@@ -9,7 +31,7 @@ __gshared byte* GPIO_BASE = cast(byte*) 0x3f200000;
 
     // register (function select) n
     byte* reg_add;
-    reg_add = cast(byte*) GPIO_BASE + 4 * q;
+    reg_add = cast(byte*) GPIO_FSEL.FSEL0 + 4 * q;
     int fn_sel_reg = *(cast(int*)reg_add);
 
     // set the (three) bits corresponding to pin
@@ -32,3 +54,4 @@ __gshared byte* GPIO_BASE = cast(byte*) 0x3f200000;
     reg_add = cast(int*) (GPIO_BASE + 0x1C + (4 * q) + (!value * 12));
     *reg_add = (1 << r);
 }
+
--- src/init/init.d
+++ src/init/init.d
@@ -10,11 +10,12 @@ extern(C) @nogc nothrow @trusted void main()
     import gpio.gpio_arm;
 
     // Set GPIO16 to OUTPUT
-    set_mode(16, 1);
-    set_mode(29, 1);
+    set_mode(16, GPIO_FSEL_MODE.OUT);
+    set_mode(29, GPIO_FSEL_MODE.OUT);
 
     bool led = false;
-    while (true) {
+    while (true)
+    {
         write(16, led);
         write(29, led);
         delay(500000);
@@ -25,6 +26,8 @@ extern(C) @nogc nothrow @trusted void main()
 
 
 
-extern(C) int _d_dso_registry() {
+extern(C) int _d_dso_registry()
+{
     return 0;
 }
+
